5179cfdafa57bba8265413accac88acc4bc83867
sorajiro0802 <sora_potato_0802@icloud.com>
2022-10-11 02:56:27 +0900

    add 'grub' detector

--- common/gesture.py
+++ common/gesture.py
@@ -1,8 +1,53 @@
 import numpy as np
 
-class detect:
-    def __init__(self, result):
-        pass
+class gestureDetector:
+    def __init__(self, result, required_time):
+        # both hand results
+        self.result = result
+        self.required_time = required_time
+        self.hand_node = None
+        self.gesture = None
+
+        
+    def detect(self):
+        if self.grub(0.1):
+            print("近い！！！")
+        else:
+            print("ちかくなーい")
+    
+    def grub(self, min_dist):
+        thumb_tip = self.hand_node[4]
+        index_finger_mcp = self.hand_node[5]
+        middle_finger_pip = self.hand_node[10]
+        ring_finger_pip = self.hand_node[14]
+        
+        dist4_5 = np.sqrt(
+            (thumb_tip.x-index_finger_mcp.x)**2 +
+            (thumb_tip.y-index_finger_mcp.y)**2 +
+            (thumb_tip.z-index_finger_mcp.z)**2
+        )
+        dist4_10 = np.sqrt(
+            (thumb_tip.x-middle_finger_pip.x)**2 +
+            (thumb_tip.y-middle_finger_pip.y)**2 +
+            (thumb_tip.z-middle_finger_pip.z)**2
+        )
+        dist4_14 = np.sqrt(
+            (thumb_tip.x-ring_finger_pip.x)**2 +
+            (thumb_tip.y-ring_finger_pip.y)**2 +
+            (thumb_tip.z-ring_finger_pip.z)**2
+        )
+        if (dist4_10 or dist4_5 or dist4_14) <= min_dist:
+            return True
+        else:
+            return False
+    
+    def updateResults(self, result):
+        self.result = result
+        self.hand_node = self.result.landmark
+
+    # test stub
+    def finger1(self):
+        print(self.result.landmark[8])
 
 if __name__=='__main__':
     pass
