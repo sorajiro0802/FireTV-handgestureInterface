0a7a357d9e3539540f99451dcb70eba4e701aa5f
sorajiro0802 <sora_potato_0802@icloud.com>
2022-10-11 03:03:47 +0900

    update to detect hand grub

--- gestureInterface.py
+++ gestureInterface.py
@@ -1,18 +1,25 @@
 #!/Users/sorakojima/proj/gestureInterface/.venv/bin/python
-import inspect
+from math import floor
 import time
 
 import cv2
 import mediapipe as mp
+from common.gesture import *
 from common.fireTV import *
+import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 mp_hands = mp.solutions.hands
 
+min_detection_time = 0.3
+detectorR = gestureDetector(result=None, required_time=min_detection_time)
+detectorL = gestureDetector(result=None, required_time=min_detection_time)
+
 # For webcam input:
 cap = cv2.VideoCapture(0)
+start_time = 0
 
 with mp_holistic.Holistic(
     min_detection_confidence=0.5,
@@ -23,6 +30,7 @@ with mp_holistic.Holistic(
             print("Ignoring empty camera frame.")
             # If loading a video, use 'break' instead of 'continue'.
             continue
+        start_time = time.time()
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
         image.flags.writeable = False
@@ -32,40 +40,37 @@ with mp_holistic.Holistic(
         # Draw the hand annotations on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        
+
         # draw hands
         if results.left_hand_landmarks:
-            mp_drawing.draw_landmarks(image,
-                                      results.left_hand_landmarks,
-                                      mp_holistic.HAND_CONNECTIONS,
-                                      mp_drawing_styles.get_default_hand_landmarks_style(),
-                                      mp_drawing_styles.get_default_hand_connections_style())
+            mp_drawing.draw_landmarks(  image,
+                                        results.left_hand_landmarks,
+                                        mp_holistic.HAND_CONNECTIONS,
+                                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                                        mp_drawing_styles.get_default_hand_connections_style())
+
         if results.right_hand_landmarks:
-            mp_drawing.draw_landmarks(image,
-                                      results.right_hand_landmarks,
-                                      mp_holistic.HAND_CONNECTIONS,
-                                      mp_drawing_styles.get_default_hand_landmarks_style(),
-                                      mp_drawing_styles.get_default_hand_connections_style())
-        """
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(image,
-                                          hand_landmarks,
-                                          mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing_styles.get_default_hand_landmarks_style(),
-                                          mp_drawing_styles.get_default_hand_connections_style()
-                                          )
-            # 人差し指の座標を表示
-            finger1 = results.multi_hand_world_landmarks[0].landmark[8]
-            print(results.multi_hand_world_landmarks[0])
-            # 動画上に文字を書き込み
-            cv2.putText(image, f"x:{finger1.x:.3f}", (1000, 30), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv2.LINE_AA)
-            cv2.putText(image, f"y:{finger1.y:.3f}", (1000, 60), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv2.LINE_AA)
-            cv2.putText(image, f"z:{finger1.z:.3f}", (1000, 90), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv2.LINE_AA)
-        """
+            mp_drawing.draw_landmarks(  image,
+                                        results.right_hand_landmarks,
+                                        mp_holistic.HAND_CONNECTIONS,
+                                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                                        mp_drawing_styles.get_default_hand_connections_style())
+            # detect right hand gesture
+            detectorR.updateResults(results.right_hand_landmarks)
+            detectorR.detect()
+
+        # display FPS
+        end_time = time.time()
+        erapsed_time = end_time - start_time
+        start_time = 0
+        cv2.putText(image, f"{floor(1/erapsed_time)}fps", (60, 60), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv2.LINE_AA)
         
         # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        #cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        # not flip the image
+        cv2.imshow('FireTV HandGesture Controller', image)
+
+        # Finish to press Esc
         if cv2.waitKey(5) & 0xFF == 27:
             break
 cap.release()
