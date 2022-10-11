56e16e6cf99a3b655c65051b6255d210ecf76a9f
sorajiro0802 <sora_potato_0802@icloud.com>
2022-10-11 18:41:13 +0900

    add method to find changed finger distance

--- common/gesture.py
+++ common/gesture.py
@@ -1,20 +1,43 @@
+from multiprocessing import Process
+import time
+
 import numpy as np
 
 class gestureDetector:
-    def __init__(self, result, required_time):
+    def __init__(self, result, required_frame):
         # both hand results
         self.result = result
-        self.required_time = required_time
+        self.required_frame = required_frame
         self.hand_node = None
         self.gesture = None
+        self.c = Counter()
+        self.a = 0
+        self.b = 0
 
-        
     def detect(self):
-        if self.grub(0.1):
-            print("ぎゅっっ")
-        else:
-            print("っっぱ")
+        # if self.grub(0.1):
+        #     print("ぎゅっっ")
+        # else:
+        #     print("っっぱ")
+        self.pointDirection(8)
     
+    def pointDirection(self, point_num):
+        if self.c.cnt == 0:
+            self.a = np.array([self.hand_node[point_num].x, self.hand_node[point_num].y, self.hand_node[point_num].z])
+            self.c.up()
+        if self.c.cnt == 10:
+            self.b = np.array([self.hand_node[point_num].x, self.hand_node[point_num].y, self.hand_node[point_num].z])  
+            self.c.clear()  
+        if self.c.cnt >= 1:
+            self.c.up()
+        try:
+            p = self.b - self.a
+            abs_p = np.linalg.norm(p, ord=2)
+            print(abs_p)
+        except:
+            pass
+        
+
     def grub(self, dist):
         thumb_tip = self.hand_node[4]
         index_finger_mcp = self.hand_node[5]
@@ -44,10 +67,26 @@ class gestureDetector:
     def updateResults(self, result):
         self.result = result
         self.hand_node = self.result.landmark
-
+    
+    def update(self, result):
+        self.updateResults(result)
+        
     # test stub
     def finger1(self):
         print(self.result.landmark[8])
+
+class Counter:
+    def __init__(self):
+        self.cnt = 0
+
+    def up(self):
+        self.cnt += 1
+    
+    def down(self):
+        self.cnt -= 1
+
+    def clear(self):
+        self.cnt = 0
 
 if __name__=='__main__':
     pass
