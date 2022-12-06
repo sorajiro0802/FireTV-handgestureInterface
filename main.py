6da3d4c47151676bcacce20eb941a6ec0c212963
sorajiro0802 <sora_potato_0802@icloud.com>
2022-12-06 15:20:46 +0900

    make stub not to use

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import cv2
 from gestureInterface import GestureInterface
 from util.fireTV import FTVController
 
-class FTVController:
-    def __init__(self, ip):
-        self.ip = ip
-    def connect(self):
-        return 0
-    def command(self, command):
-        print(command)
+# class FTVController:
+#     def __init__(self, ip):
+#         self.ip = ip
+#     def connect(self):
+#         return 0
+#     def command(self, command):
+#         print(command)
 
 if __name__=="__main__":
     # for camera input
     cap = cv2.VideoCapture(0)
     # for FireTV Connection
-    target = FTVController(ip="192.168.0.20")
+    target = FTVController(ip="172.20.10.13")
     # stop if cann not connect
     if not target.connect()==0:
         sys.exit()
@@ -42,8 +42,7 @@ if __name__=="__main__":
                         "rhand":{"grub":False, "dire":"Left"}},
                     "Home":{
                         "lhand":{"grub":False, "dire":"Up"},
-                        "rhand":{"grub":False, "dire":"Up"}
-                    }
+                        "rhand":{"grub":False, "dire":"Up"}}
                     }
     exe_commands = []
     gi = GestureInterface(cap, command_map)
