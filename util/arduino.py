7c320ff0795ba8aec0fc2e1a0ffe4c906be68c86
sorajiro0802 <sora_potato_0802@icloud.com>
2022-12-20 16:55:31 +0900

    add arduino controller

---
+++ util/arduino.py
@@ -0,0 +1,27 @@
+import serial
+
+class ArduinoController:
+    def __init__(self, port):
+        self.port = port
+        self.ser = serial.Serial(self.port, 9600)
+        self.gesture_order_map = {  'RGrub': "1",
+                                    'LGrub': "0"}
+
+    
+    def connect(self):
+        self.ser = serial.Serial(self.port, 9600)
+        return 0
+        
+    def command(self, key):
+        self.ser.write(self.gesture_order_map[key].encode())
+    
+    def kill(self):
+        print("serialPort closed")
+        self.ser.close()
+    
+    def restart(self):
+        pass
+    
+    
+if __name__=="__main__":
+    pass
