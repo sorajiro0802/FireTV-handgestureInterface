bd4b4b50c5807605b39bb1397a43656748a4d7e3
sorajiro0802 <sora_potato_0802@icloud.com>
2022-12-20 16:56:17 +0900

    fix target for controling arduino

--- main.py
+++ main.py
@@ -4,42 +4,51 @@ import threading
 import cv2
 from util.gestureInterface import GestureInterface
 from util.fireTV import FTVController
-
+from util.arduino import ArduinoController
 
 if __name__=="__main__":
     # for camera input
     cap = cv2.VideoCapture(0)
     # for FireTV Connection
-    target = FTVController(ip="192.168.0.20")
+    # target = FTVController(ip="192.168.0.20")
+    target = ArduinoController(port="/dev/cu.usbmodem101")
+    
     # stop if cann not connect
-    if not target.connect()==0:
+    if target.connect() != 0:
         sys.exit()
     # define original gesture
-    command_map = { "Up":{
+    # command_map = { "Up":{
+    #                     "lhand":{"grub":None, "dire":None},
+    #                     "rhand":{"grub":True, "dire":"Up"}},
+    #                 "Down":{
+    #                     "lhand":{"grub":None, "dire":None},
+    #                     "rhand":{"grub":True, "dire":"Down"}},
+    #                 "Left":{
+    #                     "lhand":{"grub":None, "dire":None},
+    #                     "rhand":{"grub":True, "dire":"Left"}},
+    #                 "Right":{
+    #                     "lhand":{"grub":None, "dire":None},
+    #                     "rhand":{"grub":True, "dire":"Right"}},
+    #                 "Select":{
+    #                     "lhand":{"grub":True, "dire":None},
+    #                     "rhand":{"grub":True,  "dire":None}}, 
+    #                 "Back":{
+    #                     "lhand":{"grub":None, "dire":None},
+    #                     "rhand":{"grub":False, "dire":"Left"}},
+    #                 "Home":{
+    #                     "lhand":{"grub":False, "dire":"Up"},
+    #                     "rhand":{"grub":False, "dire":"Up"}},
+    #                 "StartStop":{
+    #                     "lhand":{"grub":True, "dire":None},
+    #                     "rhand":{"grub":None, "dire":None}}
+    #                 }
+    command_map = { "RGrub":{
                         "lhand":{"grub":None, "dire":None},
-                        "rhand":{"grub":True, "dire":"Up"}},
-                    "Down":{
-                        "lhand":{"grub":None, "dire":None},
-                        "rhand":{"grub":True, "dire":"Down"}},
-                    "Left":{
-                        "lhand":{"grub":None, "dire":None},
-                        "rhand":{"grub":True, "dire":"Left"}},
-                    "Right":{
-                        "lhand":{"grub":None, "dire":None},
-                        "rhand":{"grub":True, "dire":"Right"}},
-                    "Select":{
-                        "lhand":{"grub":True, "dire":None},
-                        "rhand":{"grub":True,  "dire":None}}, 
-                    "Back":{
-                        "lhand":{"grub":None, "dire":None},
-                        "rhand":{"grub":False, "dire":"Left"}},
-                    "Home":{
-                        "lhand":{"grub":False, "dire":"Up"},
-                        "rhand":{"grub":False, "dire":"Up"}},
-                    "StartStop":{
+                        "rhand":{"grub":True, "dire":None}},
+                    "LGrub":{
                         "lhand":{"grub":True, "dire":None},
                         "rhand":{"grub":None, "dire":None}}
-                    }
+    }
     exe_commands = []
     gi = GestureInterface(cap, command_map)
     
@@ -54,3 +63,5 @@ if __name__=="__main__":
             # for not to delay capturing when command send
             exe_thread = threading.Thread(target=target.command, args=(command,), daemon=True)
             exe_thread.start()
+    
+    target.kill()
